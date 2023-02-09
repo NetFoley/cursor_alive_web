7d462d1f56cd3d89efe92880526f1dd8db3eb379
Foley Net <netfoley@gmail.com>
2023-02-09 19:42:18 +0100

    oui

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1675967893|485549620";
+const CACHE_VERSION = "1675968127|718821223";
 const CACHE_PREFIX = "Empty 4-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
